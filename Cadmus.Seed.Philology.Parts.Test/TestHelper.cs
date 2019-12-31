aa9359a70c591d9738e1a97d21e8be19a910ed65
phylax <dfusi@hotmail.com>
2019-12-31 18:11:35 +0100

    Added philology seeders test project

---
+++ Cadmus.Seed.Philology.Parts.Test/TestHelper.cs
@@ -0,0 +1,70 @@
+﻿using Cadmus.Core;
+using Cadmus.Core.Config;
+using Cadmus.Philology.Parts.Layers;
+using Cadmus.Seed.Philology.Parts.Layers;
+using Fusi.Microsoft.Extensions.Configuration.InMemoryJson;
+using Microsoft.Extensions.Configuration;
+using SimpleInjector;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using Xunit;
+
+namespace Cadmus.Seed.Parts.Test
+{
+    static internal class TestHelper
+    {
+        static public string LoadResourceText(string name)
+        {
+            if (name == null) throw new ArgumentNullException(nameof(name));
+
+            using (StreamReader reader = new StreamReader(
+                Assembly.GetExecutingAssembly().GetManifestResourceStream(
+                    $"Cadmus.Seed.Philology.Parts.Test.Assets.{name}"),
+                Encoding.UTF8))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        static public PartSeederFactory GetFactory()
+        {
+            // map
+            TagAttributeToTypeMap map = new TagAttributeToTypeMap();
+            map.Add(new[]
+            {
+                // Cadmus.Core
+                typeof(StandardItemSortKeyBuilder).Assembly,
+                // Cadmus.Philology.Parts
+                typeof(ApparatusLayerFragment).Assembly
+            });
+
+            // container
+            Container container = new Container();
+            PartSeederFactory.ConfigureServices(
+                container,
+                new StandardPartTypeProvider(map),
+                new[]
+                {
+                    // Cadmus.Seed.Philology.Parts
+                    typeof(ApparatusLayerFragmentSeeder).Assembly
+                });
+
+            // config
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .AddInMemoryJson(LoadResourceText("SeedConfig.json"));
+            var configuration = builder.Build();
+
+            return new PartSeederFactory(container, configuration);
+        }
+
+        static public void AssertPartMetadata(IPart part)
+        {
+            Assert.NotNull(part.Id);
+            Assert.NotNull(part.ItemId);
+            Assert.NotNull(part.UserId);
+            Assert.NotNull(part.CreatorId);
+        }
+    }
+}

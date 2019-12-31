558ac4edf0d5a5687cffab59ea0e980bce420e9c
phylax <dfusi@hotmail.com>
2019-12-31 16:06:57 +0100

    Added historical date part seeder test

---
+++ Cadmus.Seed.Parts.Test/HistoricalDatePartSeederTest.cs
@@ -0,0 +1,37 @@
+﻿using Cadmus.Core;
+using Cadmus.Parts.General;
+using Cadmus.Seed.Parts.General;
+using Xunit;
+
+namespace Cadmus.Seed.Parts.Test
+{
+    public sealed class HistoricalDatePartSeederTest
+    {
+        private static readonly PartSeederFactory _factory;
+        private static readonly SeedOptions _seedOptions;
+        private static readonly IItem _item;
+
+        static HistoricalDatePartSeederTest()
+        {
+            _factory = TestHelper.GetFactory();
+            _seedOptions = _factory.GetSeedOptions();
+            _item = _factory.GetItemSeeder().GetItem(1, "facet");
+        }
+
+        [Fact]
+        public void Seed_Optionless_Ok()
+        {
+            HistoricalDatePartSeeder seeder = new HistoricalDatePartSeeder();
+            seeder.SetSeedOptions(_seedOptions);
+
+            IPart part = seeder.GetPart(_item, null, _factory);
+
+            Assert.NotNull(part);
+
+            HistoricalDatePart hp = part as HistoricalDatePart;
+            Assert.NotNull(hp);
+
+            TestHelper.AssertPartMetadata(hp);
+        }
+    }
+}

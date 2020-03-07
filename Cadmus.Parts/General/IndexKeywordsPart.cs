d223ef24053b75c81a503a5d02ab3bcb75a105fa
phylax <dfusi@hotmail.com>
2020-03-07 17:23:00 +0100

    Adding index keywords part

---
+++ Cadmus.Parts/General/IndexKeywordsPart.cs
@@ -0,0 +1,92 @@
+﻿using Cadmus.Core;
+using Fusi.Tools.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cadmus.Parts.General
+{
+    [Tag("net.fusisoft.index-keywords")]
+    public sealed class IndexKeywordsPart : PartBase
+    {
+        /// <summary>
+        /// Keywords.
+        /// </summary>
+        public List<IndexKeyword> Keywords { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IndexKeywordsPart"/>
+        /// class.
+        /// </summary>
+        public IndexKeywordsPart()
+        {
+            Keywords = new List<IndexKeyword>();
+        }
+
+        /// <summary>
+        /// Adds the specified keyword. If such keywords already exist,
+        /// the old ones are replaced by the new keyword.
+        /// </summary>
+        /// <param name="keyword">The keyword.</param>
+        /// <exception cref="ArgumentNullException">keyword</exception>
+        public void AddKeyword(IndexKeyword keyword)
+        {
+            if (keyword == null) throw new ArgumentNullException(nameof(keyword));
+
+            Keywords.RemoveAll(k => k.IndexId == keyword.IndexId
+                               && k.Language == keyword.Language
+                               && k.Value == keyword.Value);
+            Keywords.Add(keyword);
+        }
+
+        /// <summary>
+        /// Get all the key=value pairs exposed by the implementor. Each key is
+        /// <c>keyword.{lang}</c> where <c>{lang}</c> is its language value,
+        /// e.g. <c>keyword.eng</c> as name and <c>sample</c> as value.
+        /// The pins are returned sorted by language and then by value.
+        /// </summary>
+        /// <returns>pins</returns>
+        public override IEnumerable<DataPin> GetDataPins()
+        {
+            if (Keywords == null || Keywords.Count == 0)
+                return Enumerable.Empty<DataPin>();
+
+            List<DataPin> pins = new List<DataPin>();
+
+            foreach (string indexId in Keywords.Select(k => k.IndexId).Distinct())
+            {
+                var keysByLang = from k in Keywords
+                                 where k.IndexId == indexId
+                                 group k by k.Language
+                                     into g
+                                 orderby g.Key
+                                 select g;
+
+                foreach (var g in keysByLang)
+                {
+                    string[] values = (from k in g
+                                       orderby k.Value
+                                       select k.Value).ToArray();
+
+                    string iid = string.IsNullOrEmpty(indexId) ? "" : "."+ indexId;
+                    pins.AddRange(from value in values
+                                  select CreateDataPin($"xkeyword.{iid}{g.Key}", value));
+                }
+            }
+
+            return pins;
+        }
+
+        /// <summary>
+        /// Converts to string.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return $"[Keywords] {Keywords.Count}";
+        }
+    }
+}
--- Cadmus.Parts/General/Keyword.cs
+++ Cadmus.Parts/General/Keyword.cs
@@ -1,45 +1,21 @@
-﻿using System;
-
-namespace Cadmus.Parts.General
+﻿namespace Cadmus.Parts.General
 {
     /// <summary>
     /// Keyword.
     /// </summary>
     /// <remarks>A keyword has an ISO 639-3 language ID and a value, and
     /// represents any relevant keyword linked to an item.</remarks>
-    public sealed class Keyword :
-        IEquatable<Keyword>, IComparable, IComparable<Keyword>
+    public class Keyword
     {
-        private string _language;
-        private string _value;
-
-        #region Properties
         /// <summary>
         /// Language (usually an ISO 639 3-letters code).
         /// </summary>
-        /// <exception cref="ArgumentNullException">null value</exception>
-        public string Language
-        {
-            get { return _language; }
-            set
-            {
-                _language = value ?? throw new ArgumentNullException(nameof(value));
-            }
-        }
+        public string Language { get; set; }
 
         /// <summary>
         /// Keyword text value.
         /// </summary>
-        /// <exception cref="ArgumentNullException">null value</exception>
-        public string Value
-        {
-            get { return _value; }
-            set
-            {
-                _value = value ?? throw new ArgumentNullException(nameof(value));
-            }
-        }
-        #endregion
+        public string Value { get; set; }
 
         /// <summary>
         /// Textual representation of this keyword.
@@ -48,172 +24,6 @@ namespace Cadmus.Parts.General
         public override string ToString()
         {
             return $"[{Language}] {Value}";
-        }
-
-        /// <summary>
-        /// Indicates whether the current object is equal to another object of
-        /// the same type.
-        /// </summary>
-        /// <returns>
-        /// true if the current object is equal to the <paramref name="other"/>
-        /// parameter; otherwise, false.
-        /// </returns>
-        /// <param name="other">An object to compare with this object.</param>
-        public bool Equals(Keyword other)
-        {
-            if (other == null) return false;
-            return Language == other.Language && Value == other.Value;
-        }
-
-        /// <summary>
-        /// Compare this keyword to another.
-        /// </summary>
-        /// <param name="other">other keyword</param>
-        /// <returns>comparison result (first by language, then by value;
-        /// case insensitive)</returns>
-        /// <exception cref="ArgumentNullException">null keyword</exception>
-        public int CompareTo(Keyword other)
-        {
-            if (other == null) throw new ArgumentNullException(nameof(other));
-
-            return Language != other.Language
-                ? string.CompareOrdinal(Language, other.Language)
-                : string.Compare(Value, other.Value, StringComparison.OrdinalIgnoreCase);
-        }
-
-        /// <summary>
-        /// Compare this keyword to another.
-        /// </summary>
-        /// <param name="obj">other keyword</param>
-        /// <returns>comparison result (first by language, then by value;
-        /// case insensitive)</returns>
-        /// <exception cref="ArgumentNullException">null keyword</exception>
-        public int CompareTo(object obj)
-        {
-            return CompareTo(obj as Keyword);
-        }
-
-        /// <summary>
-        /// Determines whether the specified <see cref="System.Object" />, is
-        /// equal to this instance.
-        /// </summary>
-        /// <param name="obj">The <see cref="System.Object" /> to compare with
-        /// this instance.</param>
-        /// <returns>
-        /// <c>true</c> if the specified <see cref="System.Object" /> is equal
-        /// to this instance; otherwise, <c>false</c>.
-        /// </returns>
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(this, obj))
-            {
-                return true;
-            }
-
-            if (obj is null)
-            {
-                return false;
-            }
-
-            Keyword other = obj as Keyword;
-            if (obj == null) return false;
-
-            return Language == other.Language && Value == other.Value;
-        }
-
-        /// <summary>
-        /// Returns a hash code for this instance.
-        /// </summary>
-        /// <returns>
-        /// A hash code for this instance, suitable for use in hashing
-        /// algorithms and data structures like a hash table. 
-        /// </returns>
-        public override int GetHashCode()
-        {
-            return ToString().GetHashCode();
-        }
-
-        /// <summary>
-        /// Implements the operator ==.
-        /// </summary>
-        /// <param name="left">The left.</param>
-        /// <param name="right">The right.</param>
-        /// <returns>
-        /// The result of the operator.
-        /// </returns>
-        public static bool operator ==(Keyword left, Keyword right)
-        {
-            if (left is null)
-            {
-                return right is null;
-            }
-
-            return left.Equals(right);
-        }
-
-        /// <summary>
-        /// Implements the operator !=.
-        /// </summary>
-        /// <param name="left">The left.</param>
-        /// <param name="right">The right.</param>
-        /// <returns>
-        /// The result of the operator.
-        /// </returns>
-        public static bool operator !=(Keyword left, Keyword right)
-        {
-            return !(left == right);
-        }
-
-        /// <summary>
-        /// Implements the operator &lt;.
-        /// </summary>
-        /// <param name="left">The left.</param>
-        /// <param name="right">The right.</param>
-        /// <returns>
-        /// The result of the operator.
-        /// </returns>
-        public static bool operator <(Keyword left, Keyword right)
-        {
-            return left is null ? right is object : left.CompareTo(right) < 0;
-        }
-
-        /// <summary>
-        /// Implements the operator &lt;=.
-        /// </summary>
-        /// <param name="left">The left.</param>
-        /// <param name="right">The right.</param>
-        /// <returns>
-        /// The result of the operator.
-        /// </returns>
-        public static bool operator <=(Keyword left, Keyword right)
-        {
-            return left is null || left.CompareTo(right) <= 0;
-        }
-
-        /// <summary>
-        /// Implements the operator &gt;.
-        /// </summary>
-        /// <param name="left">The left.</param>
-        /// <param name="right">The right.</param>
-        /// <returns>
-        /// The result of the operator.
-        /// </returns>
-        public static bool operator >(Keyword left, Keyword right)
-        {
-            return left is object && left.CompareTo(right) > 0;
-        }
-
-        /// <summary>
-        /// Implements the operator &gt;=.
-        /// </summary>
-        /// <param name="left">The left.</param>
-        /// <param name="right">The right.</param>
-        /// <returns>
-        /// The result of the operator.
-        /// </returns>
-        public static bool operator >=(Keyword left, Keyword right)
-        {
-            return left is null ? right is null : left.CompareTo(right) >= 0;
         }
     }
 }

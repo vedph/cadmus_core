6777f9a3f5cc13e09c0bcd68032f45d4f7c202ed
Myrmex <dfusi@hotmail.com>
2020-12-07 00:05:07 +0100

    Updated packages

--- InstallToFeed.bat
+++ InstallToFeed.bat
@@ -2,15 +2,15 @@
 echo PRESS ANY KEY TO INSTALL Cadmus Libraries TO LOCAL NUGET FEED
 echo Remember to generate the up-to-date package.
 pause
-c:\exe\nuget add .\Cadmus.Archive.Parts\bin\Debug\Cadmus.Archive.Parts.2.3.0.nupkg -source C:\Projects\_NuGet
-c:\exe\nuget add .\Cadmus.Core\bin\Debug\Cadmus.Core.2.3.0.nupkg -source C:\Projects\_NuGet
-c:\exe\nuget add .\Cadmus.Index\bin\Debug\Cadmus.Index.1.1.1.nupkg -source C:\Projects\_NuGet
-c:\exe\nuget add .\Cadmus.Index.Sql\bin\Debug\Cadmus.Index.Sql.1.1.2.nupkg -source C:\Projects\_NuGet
-c:\exe\nuget add .\Cadmus.Lexicon.Parts\bin\Debug\Cadmus.Lexicon.Parts.2.3.0.nupkg -source C:\Projects\_NuGet
-c:\exe\nuget add .\Cadmus.Mongo\bin\Debug\Cadmus.Mongo.2.3.3.nupkg -source C:\Projects\_NuGet
-c:\exe\nuget add .\Cadmus.Parts\bin\Debug\Cadmus.Parts.2.3.0.nupkg -source C:\Projects\_NuGet
-c:\exe\nuget add .\Cadmus.Philology.Parts\bin\Debug\Cadmus.Philology.Parts.2.3.0.nupkg -source C:\Projects\_NuGet
-c:\exe\nuget add .\Cadmus.Seed\bin\Debug\Cadmus.Seed.1.1.1.nupkg -source C:\Projects\_NuGet
-c:\exe\nuget add .\Cadmus.Seed.Parts\bin\Debug\Cadmus.Seed.Parts.1.1.1.nupkg -source C:\Projects\_NuGet
-c:\exe\nuget add .\Cadmus.Seed.Philology.Parts\bin\Debug\Cadmus.Seed.Philology.Parts.1.1.1.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Archive.Parts\bin\Debug\Cadmus.Archive.Parts.2.3.1.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Core\bin\Debug\Cadmus.Core.2.3.1.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Index\bin\Debug\Cadmus.Index.1.1.2.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Index.Sql\bin\Debug\Cadmus.Index.Sql.1.1.3.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Lexicon.Parts\bin\Debug\Cadmus.Lexicon.Parts.2.3.1.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Mongo\bin\Debug\Cadmus.Mongo.2.3.4.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Parts\bin\Debug\Cadmus.Parts.2.3.1.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Philology.Parts\bin\Debug\Cadmus.Philology.Parts.2.3.1.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Seed\bin\Debug\Cadmus.Seed.1.1.2.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Seed.Parts\bin\Debug\Cadmus.Seed.Parts.1.1.2.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Seed.Philology.Parts\bin\Debug\Cadmus.Seed.Philology.Parts.1.1.2.nupkg -source C:\Projects\_NuGet
 pause
